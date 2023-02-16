018c1145faf9af5539814d4c19cdbea788d99ae8
itstoomanymichaels <maikeruh03@gmail.com>
2023-02-16 15:41:29 -0500

    Edited scrollbar and other style changes, imported BrowserRouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import LoginForm from './Components/Login/LoginForm/LoginForm';
+import { ColorModeContext, themeSettings, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <LoginForm />
-  );
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <LoginForm />
+          <main className='content'></main>
+        </div>
+      </ThemeProvider>
+  </ColorModeContext.Provider>)
 }
 
 export default App;
